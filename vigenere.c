807f3799d60b8431961b93b1d7f5ca8afbf74958
ShelmyLin <linxiongmin@gmail.com>
2014-12-23 11:12:12 +0100

    fix malloc bugs, when the ciphertext is a bit long, there may be a malloc error, now fix it

--- vigenere.c
+++ vigenere.c
@@ -5,21 +5,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+struct Same_str{
+  char str[1000];
+  int w;
+  int dist;
+};
+
 void convert(char *origintext, char *plaintext)
 {
   int i, j;
   i = 0;
   j = 0;
-  //char *plaintext = (char*) malloc(100);
   memset(plaintext, '\0', strlen(plaintext));
   while(origintext[i] != '\0')
   {
-    
-    //if((j+1) % 100 == 0)
-    //{
-      //plaintext = (char*)realloc(plaintext, strlen(plaintext) + 100);
-      //memset(&plaintext[j], '\0', 100);
-    //}
     if(origintext[i] <= 'Z' && origintext[i] >= 'A')
     {
       plaintext[j] = origintext[i] + 32;
@@ -41,17 +41,22 @@ void encrypt(char *plaintext, char *key, char *ciphertext)
   int offset ;
   i =    0   ;
   j =    0   ;
-  while(plaintext[i] != '\n' && plaintext[i] != '\0')
+
+  while( plaintext[i] != '\n' &&
+         plaintext[i] != '\0'   )
   {
     offset = key[j] -(int)('a');
-    if(plaintext[i] <= 'Z' && plaintext[i] >='A')
+
+    if(  plaintext[i] <= 'Z'  &&
+         plaintext[i] >='A'     )
     {
       plaintext[i] = plaintext[i] + 32 ;
     }
     ciphertext[i] = (int)('a') + (plaintext[i] - (int)'a' + offset) % 26;
     i++;
     j++;
-    if(key[j] == '\n' || key[j] == '\0')
+    if( key[j] == '\n' ||
+        key[j] == '\0'   )
     {
       j = 0;
     }
@@ -67,9 +72,6 @@ void get_divisor(int *p, int num)
     if(num%i == 0)
     {
       p[i-1] = 1;
-      //j = num / i;
-      //p[j-1] = 1;
-     // printf("get %d %d\n", i , j);
     }
   }
   
@@ -77,48 +79,72 @@ void get_divisor(int *p, int num)
 
 /* to avoid noice, get_key_size() calculates the weight of each possible keys and the result is    *
  * more precise than get_key_size_02, which just calculates the same divisor of all possible keys */
+/* the way i was used to calculate the weight was wrong, i should calculate the weight of same blocks, *
+ * not divisors*/
 int get_key_size(char* ciphertext)
 {
   int size = 0;
-  int i;
-  int m;
-  int j, k;
+  int i, j, m, k, r;
   int len = strlen(ciphertext);
   int *keysize = malloc(sizeof(int)*strlen(ciphertext));
-  memset(keysize, 0, sizeof(int)*strlen(ciphertext)); // if possible key size is 27, set keysize[26]=1; 
-  k = 0; //the count of possible key_size
+  memset(keysize, 0, sizeof(int)*strlen(ciphertext));    // if possible key size is 27, set keysize[26]=1; 
+  k = 0;                                                 //the count of possible key_size
   
+  //struct Same_str same_str[1000]; // malloc
+  //for( i = 0; i < 1000; i++)
+  //{
+  //  memset(same_str[i].str, '\0', strlen(same_str[i].str));
+  //  same_str[i].w    = 0;
+  //  same_str[i].dist = 0; 
+  //}
+   
+  char *block_1 = malloc(len);
+  char *block_2 = malloc(len);
   for(m = 2; m < len/2 ; m++)
   {
     int block_num = len -m;
     for(i = 0; i < block_num; i++)
     {
-      char *block_1 = malloc(sizeof(char)*m + 2);
+      //char *block_1 = malloc(sizeof(char)*m + 2);
       memset(block_1, '\0', strlen(block_1));
       memcpy(block_1, ciphertext + i, m+1);
+      int w = 0;                                           // weight
       for(j = i + 1 + m; j < block_num; j++)
       {
         if(j >= block_num) break;
-        char *block_2 = malloc(sizeof(char)*m + 2);
+        //char *block_2 = malloc(sizeof(char)*m + 2);
         memset(block_2, '\0', strlen(block_2));
         memcpy(block_2, ciphertext + j, m+1);
         if(strcmp(block_1, block_2) == 0)
         {
            printf("found same block: %s at %d and %d, size: %d\n", block_1, i, j, j-i);
            keysize[k] = j-i;
+           w++;
+           //strcpy(same_str[k].str, block_1);
+           //same_str[k].w    = w;
+           //same_str[k].dist = j - i; 
            k++;
+
         }
-        free(block_2);
-        //usleep(1);
-        printf("here, m = %d i = %d j = %d\n", m ,i ,j);
+       // free(block_2);
       }
-      free(block_1);
+      if(w > 0)
+      {
+        printf("------>>>>>>found same block: %s, %d times\n",block_1,  w);
+      }
+      //free(block_1);
       usleep(1);
-      printf("here, m = %d i = %d j = %d\n", m ,i ,j);
     }
-    printf("here, m = %d i = %d j = %d\n", m ,i ,j);
   }
-  printf("here, m = %d i = %d j = %d\n", m ,i ,j);
+
+  free(block_1);
+  free(block_2);
+
+
+  /* clean same_str, find same block str, and add its weight*/
+
+
+  //printf("here, m = %d i = %d j = %d\n", m ,i ,j);
   /* from code above, we know all possible key sizes (count: k), which are stored in keysize array *
    * if possible key size is 27, then keysize[26]=1;                                               */
 
@@ -127,22 +153,24 @@ int get_key_size(char* ciphertext)
    * array ptr[i] and be added with an all-0 array: weight, from the weight of each key, we know   *
    * the most possible keysize                                                                     */
   int **ptr = malloc(sizeof(int*)*k);
-  int max = 0;
+  int max   = 0;
   for(i = 0; i < k; i++)
   {
      int num = keysize[i];
-     max = (max > num)? max : num;
+     max     = (max > num)? max : num;
   }
 
   int *weight = malloc(sizeof(int) * max);
-  memset(weight, 0, max);
-
+  for(i = 0; i < max ;i++)
+  {
+    weight[i] = 0;
+  }
   for(i = 0; i < k; i++)
   {
-     int num = keysize[i];
-     ptr[i] = malloc(sizeof(int)*max);
+     int num       = keysize[i];
+     ptr[i]        = malloc(sizeof(int)*max);
+     ptr[i][num-1] = 1 ;
      memset(ptr[i], 0, max);
-     ptr[i][num-1]   = 1 ;
 
      /* at first, ptr[i] = "00000...00100..", after get_divisor, ptr[i] = "0..1..00..100..1..100...", *
      * where 1 means the match pos is the divisor number of keysize[i]                               */
@@ -156,10 +184,13 @@ int get_key_size(char* ciphertext)
   
   for(j = 0; j < max; j++)
   {
-    printf("weight of %d: %d\n", j+1, weight[j]);
+    if(weight[j] != 0)
+    {
+      printf("weight of %d: %d\n", j+1, weight[j]);
+    }
     if(max_weight < weight[j])
     {
-      size = j+1;
+      size       = j+1;
       max_weight = weight[j];
     }
   }
@@ -203,12 +234,6 @@ void decrypt(char *ciphertext, char *key, char* plaintext)
   }
 
 }
-void modify_plaintext(char *text)
-{
-  int i, j;
-  i = 0;
-//  while(text[i] != '\0')
-}
 int main()
 {
   //char *plaintext = "helloisimaiamveryhappytoliveinthisverybeautifulcampusthisisaverygoodplace";// if declare in this way , i can't modify it
@@ -228,7 +253,6 @@ int main()
       origintext = (char*)realloc(origintext, strlen(origintext) + 100);
     }
     
-    //plaintext = (char*)realloc(plaintext, strlen(plaintext)+1);
   }
   origintext[i] = '\0';
   plaintext = (char*)malloc(strlen(origintext));   
